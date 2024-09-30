a9224c0076a798b6672f376844b419e6f861dfdd
TimurEfimov <tefimov85@gmail.com>
2024-09-30 14:03:24 +0300

    Header

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <the-header></the-header>
+  </div>
+</template>
+
+<script>
+import theHeader from "./components/theHeader.vue";
+
+export default {
+  components: { theHeader },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/theHeader.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="header__background">
+    <header class="header__container">
+      <div class="logo">
+        <a href="#" class="logo__text">YourMeal</a>
+        <img src="./../assets/logo.png" alt="" class="logo__png" />
+      </div>
+      <div class="welcome__text">
+        <img src="./../assets/pic.png" alt="" />
+        <div class="welcome__intext">
+          <h1 class="welcome__title">
+            Только самые <span>сочные бургеры!</span>
+          </h1>
+          <p class="welcome__delivery">Бесплатная доставка от 599₽</p>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* Start logo */
+.header__background {
+  background-image: url("./../assets/bg.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center, right bottom;
+  
+}
+
+.header__container {
+  max-width: 785px;
+  box-sizing: border-box;
+  margin: 0 auto;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+  padding: 22px 315px;
+}
+
+.logo__text {
+  color: #fff;
+  font-weight: 700;
+  font-size: 24px;
+}
+
+.logo__png {
+  padding-left: 4px;
+}
+/* Finish logo */
+
+/* Start welcome */
+.welcome__text {
+  display: flex;
+  padding-bottom: 63px;
+}
+
+.welcome__title {
+  font-size: 50px;
+  font-weight: 800;
+  color: #fff;
+  line-height: 120%;
+}
+span {
+  color: #ff5c00;
+}
+
+.welcome__intext {
+  display: flex;
+  flex-direction: column;
+  margin-left: 21px;
+  padding-top: 46px;
+}
+
+.welcome__delivery {
+  color: #fff;
+  margin-top: 52px;
+  font-size: 16px;
+}
+/* Finish welcome */
+</style>
